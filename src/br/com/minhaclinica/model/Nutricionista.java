14a1363ac9e969dbdc9cf4bb106e2fc11b673df1
FernandoBack <fernandoback@gmail.com>
2025-10-18 14:13:18 -0300

    feat: Adiciona classes Funcionario e Nutricionista com validação (Fail-Fast)

--- src/br/com/minhaclinica/model/Nutricionista.java
+++ src/br/com/minhaclinica/model/Nutricionista.java
@@ -1,4 +1,38 @@
 package br.com.minhaclinica.model;
 
-public class Nutricionista {
-}
+
+    public class Nutricionista extends Funcionario{
+
+        private int numeroConsulta;
+
+        public Nutricionista(){
+            super();
+            this.numeroConsulta = 0;
+
+        }
+        public Nutricionista(String nome, int idade, double salario, int numeroConsulta){
+            super (nome, idade, salario);
+            this.setNumeroConsulta(numeroConsulta);
+        }
+
+        public int getNumeroConsulta() {
+            return numeroConsulta;
+        }
+        /**
+         * @param numeroConsultas O número (deve ser >= 0).
+         * @throws IllegalArgumentException Se o número for inválido.
+         */
+        public void setNumeroConsulta(int numeroConsulta) {
+            if (numeroConsulta >= 0){
+            } else {
+                throw new IllegalArgumentException("O número de consulta não pode ser negativo");
+            }
+        }
+        @Override
+        public String toString(){
+            String infoFuncionario = super.toString().replace("]", "");
+            return String.format("%s, Consultas: %d]",
+            infoFuncionario, this.numeroConsulta);
+        }
+    }
+
