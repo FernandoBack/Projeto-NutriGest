08cef55469eb4f769fd0ce50a3442eaf1d1d9dea
FernandoBack <fernandoback@gmail.com>
2025-10-21 10:44:41 -0300

    refactor: Introduz IConsultaManager e migra ConsultasImpl para Singleton

--- src/br/com/minhaclinica/repository/IConsultaManager.java
+++ src/br/com/minhaclinica/repository/IConsultaManager.java
@@ -1,4 +1,23 @@
 package br.com.minhaclinica.repository;
 
-public class IConsultaManager {
+import br.com.minhaclinica.model.Consulta;
+import java.util.List;
+
+
+public interface IConsultaManager {
+
+    /**
+     * @param consulta A consulta a ser adicionada.
+     */
+    void AdicionaConsulta(Consulta consulta);
+
+    /**
+     * @return Uma List<Consulta> (preferencialmente não modificável).
+     */
+    List<Consulta> RetornaConsultas();
+
+    /**
+     * @param posicao O índice (baseado em 0) da consulta a ser removida.
+     */
+    void DeletaConsulta(int posicao);
 }
